61e90509731cf84067930400ac9491016f31e846
Sachin Bhosle <sachinbhosle542@gmail.com>
2026-04-03 11:32:46 +0530

    feat: Update offsets and constants for client.dll and engine2.dll modules

--- output/interfaces.hpp
+++ output/interfaces.hpp
@@ -1,10 +1,9 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-27 23:29:07.271766300 UTC
+// 2026-04-03 06:01:03.047358900 UTC
 
 #pragma once
 
 #include <cstddef>
-#include <cstdint>
 
 namespace cs2_dumper {
     namespace interfaces {
@@ -15,14 +14,14 @@ namespace cs2_dumper {
         }
         // Module: client.dll
         namespace client_dll {
-            constexpr std::ptrdiff_t ClientToolsInfo_001 = 0x2062F90;
-            constexpr std::ptrdiff_t EmptyWorldService001_Client = 0x201CE60;
-            constexpr std::ptrdiff_t GameClientExports001 = 0x205FC70;
-            constexpr std::ptrdiff_t LegacyGameUI001 = 0x207D4D0;
-            constexpr std::ptrdiff_t Source2Client002 = 0x2309DD0;
-            constexpr std::ptrdiff_t Source2ClientConfig001 = 0x228DF00;
-            constexpr std::ptrdiff_t Source2ClientPrediction001 = 0x206A8F0;
-            constexpr std::ptrdiff_t Source2ClientUI001 = 0x207BD60;
+            constexpr std::ptrdiff_t ClientToolsInfo_001 = 0x2065F90;
+            constexpr std::ptrdiff_t EmptyWorldService001_Client = 0x201FE50;
+            constexpr std::ptrdiff_t GameClientExports001 = 0x2062C70;
+            constexpr std::ptrdiff_t LegacyGameUI001 = 0x20804D0;
+            constexpr std::ptrdiff_t Source2Client002 = 0x230CDD0;
+            constexpr std::ptrdiff_t Source2ClientConfig001 = 0x2290F00;
+            constexpr std::ptrdiff_t Source2ClientPrediction001 = 0x206D8F0;
+            constexpr std::ptrdiff_t Source2ClientUI001 = 0x207ED60;
         }
         // Module: engine2.dll
         namespace engine2_dll {
@@ -187,6 +186,7 @@ namespace cs2_dumper {
         }
         // Module: steamclient64.dll
         namespace steamclient64_dll {
+            constexpr std::ptrdiff_t CLIENTENGINE_INTERFACE_VERSION005 = 0xFFFFFFFF8BB6677A;
             constexpr std::ptrdiff_t IVALIDATE001 = 0x16998B8;
             constexpr std::ptrdiff_t SteamClient006 = 0x1696D50;
             constexpr std::ptrdiff_t SteamClient007 = 0x1696D58;
@@ -219,10 +219,6 @@ namespace cs2_dumper {
         // Module: v8system.dll
         namespace v8system_dll {
             constexpr std::ptrdiff_t Source2V8System001 = 0x316B0;
-        }
-        // Module: vconcomm.dll
-        namespace vconcomm_dll {
-            constexpr std::ptrdiff_t VConComm001 = 0x39540;
         }
         // Module: vphysics2.dll
         namespace vphysics2_dll {
--- output/offsets.cs
+++ output/offsets.cs
@@ -1,36 +1,32 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-03-27 23:29:07.271766300 UTC
+// 2026-04-03 06:01:03.047358900 UTC
 
 namespace CS2Dumper.Offsets {
     // Module: client.dll
     public static class ClientDll {
-        public const nint dwCSGOInput = 0x231B2E0;
-        public const nint dwEntityList = 0x24B0258;
-        public const nint dwGameEntitySystem = 0x24B0258;
+        public const nint dwCSGOInput = 0x231E330;
+        public const nint dwEntityList = 0x21CFEC8;
         public const nint dwGameEntitySystem_highestEntityIndex = 0x20A0;
-        public const nint dwGameRules = 0x230EED0;
-        public const nint dwGlobalVars = 0x205F540;
-        public const nint dwGlowManager = 0x230BCD8;
-        public const nint dwLocalPlayerController = 0x22F5028;
-        public const nint dwLocalPlayerPawn = 0x206A9E0;
-        public const nint dwPlantedC4 = 0x2318A60;
-        public const nint dwPrediction = 0x206A8F0;
-        public const nint dwSensitivity = 0x230C7E8;
+        public const nint dwGameRules = 0x2311ED0;
+        public const nint dwGlobalVars = 0x2062540;
+        public const nint dwGlowManager = 0x230ECD8;
+        public const nint dwLocalPlayerController = 0x22F8028;
+        public const nint dwLocalPlayerPawn = 0x206D9E0;
+        public const nint dwPrediction = 0x206D8F0;
+        public const nint dwSensitivity = 0x230F7E8;
         public const nint dwSensitivity_sensitivity = 0x58;
-        public const nint dwViewAngles = 0x231B968;
-        public const nint dwViewMatrix = 0x2310F10;
-        public const nint dwViewRender = 0x2311328;
-        public const nint dwWeaponC4 = 0x229A2B0;
+        public const nint dwViewAngles = 0x231E9B8;
+        public const nint dwViewMatrix = 0x2313F10;
+        public const nint dwViewRender = 0x2314328;
+        public const nint dwWeaponC4 = 0x229D2B0;
     }
     // Module: engine2.dll
     public static class Engine2Dll {
         public const nint dwBuildNumber = 0x60E514;
-        public const nint dwNetworkGameClient = 0x9095D0;
         public const nint dwNetworkGameClient_clientTickCount = 0x378;
         public const nint dwNetworkGameClient_deltaTick = 0x24C;
         public const nint dwNetworkGameClient_isBackgroundMap = 0x2C141F;
         public const nint dwNetworkGameClient_localPlayer = 0xF8;
-        public const nint dwNetworkGameClient_maxClients = 0x240;
         public const nint dwNetworkGameClient_serverTickCount = 0x24C;
         public const nint dwNetworkGameClient_signOnState = 0x230;
         public const nint dwWindowHeight = 0x90D99C;
@@ -47,6 +43,5 @@ namespace CS2Dumper.Offsets {
     // Module: soundsystem.dll
     public static class SoundsystemDll {
         public const nint dwSoundSystem = 0x4F3470;
-        public const nint dwSoundSystem_engineViewData = 0x7C;
     }
 }
