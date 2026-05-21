5d92014f3069f7408f63369595c6ab1ead530d52
Sachin Bhosle <sachinbhosle542@gmail.com>
2026-05-21 21:05:12 +0530

    Update offsets and interface definitions across multiple modules
    - Adjusted offsets for Source2ClientConfig001, EmptyWorldService001_Server, NavGameTest001, Source2ServerConfig001, dwLocalPlayerController, and dwLocalPlayerController in various files.
    - Updated interface definitions in interfaces.json, interfaces.rs, interfaces.zig, materialsystem2_dll, networksystem_dll, offsets, panorama_dll, particles_dll, pulse_system_dll, rendersystemdx11_dll, resourcesystem_dll, scenesystem_dll, schemasystem_dll, server_dll, soundsystem_dll, steamaudio_dll, vphysics2_dll, and worldrenderer_dll.
    - All changes reflect the latest generated data as of 2026-05-21.

--- output/offsets.cs
+++ output/offsets.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-19 06:12:57.807442300 UTC
+// 2026-05-21 15:34:25.441974100 UTC
 
 namespace CS2Dumper.Offsets {
     // Module: client.dll
@@ -11,7 +11,7 @@ namespace CS2Dumper.Offsets {
         public const nint dwGameRules = 0x2366788;
         public const nint dwGlobalVars = 0x2085788;
         public const nint dwGlowManager = 0x2363580;
-        public const nint dwLocalPlayerController = 0x2345D50;
+        public const nint dwLocalPlayerController = 0x2345D60;
         public const nint dwLocalPlayerPawn = 0x2090880;
         public const nint dwPlantedC4 = 0x2374278;
         public const nint dwPrediction = 0x2090790;
--- output/particles_dll.hpp
+++ output/particles_dll.hpp
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-19 06:12:57.807442300 UTC
+// 2026-05-21 15:34:25.441974100 UTC
 
 #pragma once
 
