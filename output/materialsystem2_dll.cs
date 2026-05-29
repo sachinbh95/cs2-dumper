daa21b848f452083e72b76663ed77b014bfe534b
Sachin Bhosle <sachinbhosle542@gmail.com>
2026-05-29 21:02:05 +0530

    Update generated files with new offsets and timestamps
    - Updated timestamps in all generated files to 2026-05-29.
    - Adjusted offsets in `offsets.cs`, `offsets.hpp`, `offsets.rs`, `offsets.zig`, and `offsets.json` for `client.dll` module.
    - Updated corresponding offsets in `networksystem_dll`, `panorama_dll`, `particles_dll`, `pulse_system_dll`, `rendersystemdx11_dll`, `resourcesystem_dll`, `scenesystem_dll`, `schemasystem_dll`, `server_dll`, `soundsystem_dll`, `steamaudio_dll`, `vphysics2_dll`, and `worldrenderer_dll`.

--- output/materialsystem2_dll.cs
+++ output/materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-22 15:42:40.877310900 UTC
+// 2026-05-29 15:30:53.995677200 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
--- output/steamaudio_dll.rs
+++ output/steamaudio_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-22 15:42:40.877310900 UTC
+// 2026-05-29 15:30:53.995677200 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
