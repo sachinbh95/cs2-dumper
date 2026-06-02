95fbae43b316ace964f4689b2a24aad06fa4dcb9
Sachin Bhosle <sachinbhosle542@gmail.com>
2026-06-02 21:09:00 +0530

    Update generated files with new timestamps and adjusted offsets for various modules
    - Updated timestamps in header files for networksystem_dll, panorama_dll, particles_dll, pulse_system_dll, rendersystemdx11_dll, resourcesystem_dll, scenesystem_dll, schemasystem_dll, server_dll, soundsystem_dll, steamaudio_dll, vphysics2_dll, and worldrenderer_dll to reflect the latest generation date.
    - Adjusted offsets in offsets.cs, offsets.hpp, offsets.rs, and offsets.zig for client.dll and engine2.dll modules to correct memory addresses.
    - Ensured consistency across all generated files for accurate integration with the CS2Dumper tool.

--- output/vphysics2_dll.cs
+++ output/vphysics2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-05-29 15:30:53.995677200 UTC
+// 2026-06-02 15:37:38.936916300 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: vphysics2.dll
