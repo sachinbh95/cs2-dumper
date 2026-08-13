c71a8fbc9d94826164b2501b602ab9ce741664c6
Sachin Bhosle <sachinbhosle542@gmail.com>
2026-08-13 11:26:26 +0530

    Update generated files with new timestamps and corrected offsets
    - Updated timestamps in all generated files to reflect the latest generation date (2026-08-13).
    - Corrected offsets for `dwLocalPlayerController`, `dwWeaponC4`, `dwNetworkGameClient`, `dwWindowHeight`, and `dwWindowWidth` in various modules.

--- output/buttons.rs
+++ output/buttons.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-11 05:23:02.893107 UTC
+// 2026-08-13 05:54:39.188446100 UTC
 
 #![allow(non_upper_case_globals, unused)]
 
--- output/scenesystem_dll.cs
+++ output/scenesystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-11 05:23:02.893107 UTC
+// 2026-08-13 05:54:39.188446100 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: scenesystem.dll
